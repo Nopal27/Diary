4f4e554e7ba4431d7ce7ecfd4f3bdde1cb381bef
fachriraihan <113666227+fachriraihan@users.noreply.github.com>
2023-03-14 11:27:41 +0700

    Update

--- app.py
+++ app.py
@@ -17,43 +17,48 @@ client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
-    articles = list(db.diary.find({},{'_id':False}))
-    return jsonify({ 'articles': articles })
+    articles = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'articles': articles})
+
 
 @app.route('/diary', methods=['POST'])
 def save_diary():
-    title_receive = request.form["title_give"]
-    content_receive = request.form["content_give"]
-    
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
     today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
 
     file = request.files['file_give']
     extension = file.filename.split('.')[-1]
-    filename = f'file-{mytime}.{extension}'
-    save_to = f'static/{filename}'
-    file.save(save_to)
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
 
     profile = request.files['profile_give']
-    extension = profile.filename.split('.')[-1]
-    profilename = f'profile-{mytime}.{extension}'
-    save = f'static/{profilename}'
-    profile.save(save)
+    extension = file.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
+
+    time = today.strftime('%Y.%m.%d')
 
     doc = {
-        'file' : filename,    
+        'file': filename,
         'profile' : profilename,
         'title': title_receive,
         'content': content_receive,
+        'time': time,
     }
     db.diary.insert_one(doc)
-    return jsonify({'msg':'Upload complete!'})
+
+    return jsonify({"message": "Upload Complete!"})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
